304cb2b218f3409f107e965892fafe5ed0eccc6f
Gülsüm Sevil Yavuz <gulsumyavuz54@gmail.com>
2023-06-27 20:40:31 +0200

    first commit

---
+++ src/test/java/stepdefinitions/MRoomStepDefinitionsedunna.java
@@ -0,0 +1,11 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.When;
+
+public class MRoomStepDefinitionsedunna {
+
+    @When("Click on ItemsAndTitles")
+    public void click_on_items_and_titles() {
+
+
+    }}
